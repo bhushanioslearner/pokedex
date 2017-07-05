b9b8ea69c1ffc45933a58cab25b718a256c63031
Bhushan Shinde <bhushanshinde@administrators-MacBook-Pro.local>
2017-07-05 12:33:58 +0530

    First Commit

---
+++ PokDexApp/PokDexApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokDexApp
+//
+//  Created by Bhushan Shinde on 05/07/17.
+//  Copyright © 2017 Bhushan Shinde. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
